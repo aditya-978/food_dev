b20546c7a2f2bbd8f778a8436904d3ce70230ce0
aditya-978 <adityajha978@gmail.com>
2024-01-07 16:05:22 +0530

    final upload

--- food_dev/src/screens/MyOrder.js
+++ food_dev/src/screens/MyOrder.js
@@ -1,43 +1,66 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 export default function MyOrder() {
     const [orderData, setOrderData] = useState("");
-  return (
-    <>
-        <div>
-            <Navbar/>
-        </div>
-        {/* <div className='container'>
-            <div className='row'>
-                {orderData.length !== 0 ? Array(orderData).map(data =>{
-                    return(
-                        data.orderData ? data.orderData.order_data.slice(0).reverse().map((item)=>{
-                            return(
-                                item.map((arrayData)=>{
-                                    return(
-                                        <div>
-                                            {arrayData.Order_date? <div className='m-auto mt-5'>{data = arrayData.Order_date}<hr/></div>:
-                                            <div className='col-12 col-md-6 col-lg-3'>
-                                                <div className='card mt-3' style={{width: '16rem', maxHeight:'360px'}}>
-                                                    <div className='card-body'>
-                                                        <h5 className='card-title'></h5>
-                                                    </div>
-                                                </div>
-                                            </div>}
-                                        </div>
-                                    )
-                                })
-                            )
-                        })
-                    )
-                })}
+    const fetchMyOrder = async() => {
+        await fetch("http://localhost:5000/api/myOrderedData",{
+            method:"POST",
+            headers:{'Content-Type':'application/json'},
+            body: JSON.stringify({
+                email: localStorage.getItem('userEmail')
+            })
+        }).then(async(res) => {
+            let response = await res.json()
+            await setOrderData(response)
+        })
+    }
+    useEffect(()=>{
+        fetchMyOrder()
+    },[])
+
+    return (
+        <>
+            <div>
+                <Navbar />
             </div>
-        </div> */}
-        <div>
-            <Footer/>
-        </div>
-    </>
-  )
+            <div className='container'>
+                <div className='row'>
+                    {orderData.length !== 0 ? Array(orderData).map(data => {
+                        return (
+                            data.orderData ? data.orderData.order_data.slice(0).reverse().map((item) => {
+                                return (
+                                    item.map((arrayData) => {
+                                        return (
+                                            <div>
+                                                {arrayData.Order_date ? <div className='m-auto mt-5'>{data = arrayData.Order_date}<hr /></div> :
+                                                    <div className='col-12 col-md-6 col-lg-3'>
+                                                        <div className='card mt-3' style={{ width: '16rem', maxHeight: '360px' }}>
+                                                            {/* <img src={arrayData.img} className='card-img-top' alt='...' /> */}
+                                                            <div className='card-body'>
+                                                                <h5 className='card-title'>{arrayData.name}</h5>
+                                                                <div className='container w-100 p-0' style={{ height: '30px' }}>
+                                                                    <span className='m-1'>{arrayData.qty}</span>
+                                                                    <span className='m-1'>{arrayData.size}</span>
+                                                                    <span className='m-1'>{data}</span>
+                                                                    <div className='d-inline ms-2 h-100 w-20 fs-5'>{arrayData.price}/-</div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>}
+                                            </div>
+                                        )
+                                    })
+                                )
+                            }) : ''
+                        )
+                    }) : ''}
+                </div>
+            </div>
+            <div>
+                <Footer />
+            </div>
+        </>
+    )
 }
