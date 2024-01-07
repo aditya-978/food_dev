cb386d011cfaa48198335e9cc77314d710cd815a
aditya-978 <adityajha978@gmail.com>
2024-01-07 14:12:21 +0530

    little_changes_in_index.js

--- food_dev/backend/index.js
+++ food_dev/backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+
 const port = 5000
+
 require('dotenv').config();
+
 const mongoDb = require ('./db')
 mongoDb();
+
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-origin","http://localhost:3000");
     res.header(
@@ -12,9 +16,11 @@ app.use((req,res,next)=>{
     )
     next();
 })
+
 app.get('/', (req,res)=>{
     res.send('Hello World!')
 })
+
 app.use(express.json())
 app.use('/api', require("./routes/CreateUser"));
 app.use('/api', require("./routes/DisplayData"));
